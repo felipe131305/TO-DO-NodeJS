6f85c1ba2b9bb402fefe0347a4d0369f16cde147
felipe131305 <felipe08119617@gmail.com>
2023-11-05 13:34:40 -0500

    se realiza CRUD de tareas pra cada usuario

---
+++ src/controllers/tasks.controller.js
@@ -0,0 +1,40 @@
+import Task from "../models/task.model.js";
+
+export const getTasks = async (req, res) => {
+  const tasks = await Task.find({
+    user: req.user.id
+  });
+  res.json(tasks);
+};
+
+export const createTask = async (req, res) => {
+  const { title, description, date } = req.body;
+  const newTask = new Task({
+    title,
+    description,
+    date,
+    user: req.user.id,
+  });
+  const saveTask = await newTask.save();
+  res.json(saveTask);
+};
+
+export const getTask = async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  if (!task) return res.status(400).json({ message: "Task not found" });
+  res.json(task);
+};
+
+export const updateTask = async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.dody, {
+    new: true,
+  });
+  if (!task) return res.status(400).json({ message: "Task not update" });
+  res.json(task);
+};
+
+export const deleteask = async (req, res) => {
+  const task = await Task.findByIdAndDelete(req.params.id);
+  if (!task) return res.status(400).json({ message: "Task not delete" });
+  res.json(task);
+};
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import {authRequired} from "../middlewares/validateToken.js";
-
+import { authRequired } from "../middlewares/validateToken.js";
+import {
+  getTasks,
+  createTask,
+  getTask,
+  deleteask,
+  updateTask,
+} from "../controllers/tasks.controller.js";
 const router = Router();
 
-router.get("/tasks", authRequired, (req, res) => {
-  res.send("Task");
-});
+router.get("/tasks", authRequired,getTasks);
+router.post("/tasks", authRequired,createTask);
+router.delete("/tasks/:id", authRequired,deleteask);
+router.get("/tasks/:id", authRequired,getTask);
+router.put("/tasks/:id", authRequired,updateTask);
 
 export default router;
