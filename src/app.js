7f2a2dbff4ce3fd3e15f5454eb907738705f810a
felipe131305 <felipe08119617@gmail.com>
2023-11-13 16:56:16 -0500

    se realizan cambios las respeustas de los errores

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 import taksRoutes from"./routes/tasks.routes.js"
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
