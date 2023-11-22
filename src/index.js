5e4f1f4f3dd8f8063c17706c8067cdd19e29d7b2
felipe131305 <felipe08119617@gmail.com>
2023-11-21 19:04:01 -0500

    Se actualiza comando de ejecucion

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import	app from "./app.js"
 import {connectDB} from "./db.js"
+import dotenv from "dotenv"
+dotenv.config()
 
 connectDB();
-app.listen(4000,"0.0.0.0");
-console.log("Server is running on port 4000");
+app.listen(process.env.PORT,"0.0.0.0");
+console.log("Server is running on port: "+process.env.PORT);
